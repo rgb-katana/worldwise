92b1fa2330715404f2a4b6516e613a1405023f58
rgb-katana <komarova1610@yandex.ru>
2023-08-13 21:55:00 +0300

    use contextAPI to provide context to cities

---
+++ src/Contexts/CitiesContext.jsx
@@ -0,0 +1,41 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+
+const CitiesContext = createContext();
+
+const BASE_URL = 'http://localhost:8000';
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        alert('Error');
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider value={{ cities, isLoading }}>
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined) throw new Error('Context used outside provider');
+  return context;
+}
+
+export { CitiesProvider, useCities };
--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -2,8 +2,11 @@ import Message from './Message';
 import styles from './CountryList.module.css';
 import Spinner from './Spinner';
 import CountryItem from './CountryItem';
+import { useCities } from '../Contexts/CitiesContext';
 
-function CountryList({ cities, isLoading }) {
+function CountryList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
